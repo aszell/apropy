981d7e5f0f5586dda23117af1b9a4ebae2069d4d
Andris <szell.andris@gmail.com>
2016-10-20 21:10:30 +0200

    Unicode escape sequence conversions.

--- prop.py
+++ prop.py
@@ -1,7 +1,33 @@
+import re
 from collections import OrderedDict
 from namedlist import namedlist
 
 TransItem = namedlist('TransItem', 'key, comment, trans', default='')
+
+def unidecode(stre):
+    stnew = stre.group(0).decode('unicode-escape')
+    #print "re: ", stre.group(0), stnew, hexlify(stre.group(0))
+    return stnew
+
+def to_unicode(st):
+    # first convert normal accented latin-1 string to unicode
+    st = st.decode('latin-1')
+    # then convert unicode escapes to unicode in the unicode string
+    # (done so to avoid various python 2.7 error messages regarding encoding)
+    rest = re.sub(r'\\u[0-9a-fA-F]{4}', unidecode, st)
+    return rest
+
+def from_unicode(ust):
+    st = ''
+    for uch in ust:
+        res =  uch.encode('iso-8859-1', "xmlcharrefreplace")
+        #print res, len(res)
+        if len(res) > 1:
+            assert(res[0:2] == '&#')
+            st += '\\u%04x' % int(res[2:-1])
+        else:
+            st += res
+    return st
 
 def isemptyline(st):
     return not st.strip()
@@ -33,6 +59,7 @@ def propread(fhnd):
     tdict = OrderedDict()
 
     for l in fhnd:
+        l = to_unicode(l)
         if state == MULTILINE:
             if not ismultiline(l):
                 state = COMMENTS
@@ -41,7 +68,7 @@ def propread(fhnd):
                 item = TransItem()
             else:
                 item.trans += l
-        if state == COMMENTS:
+        elif state == COMMENTS:
             if iscomment(l) or isemptyline(l):
                 item.comment += l
             elif iskey(l):
@@ -58,14 +85,17 @@ def propread(fhnd):
 def propsave(fhnd, propdict):
     for key, item in propdict.items():
         fhnd.write(item.comment)
-        fhnd.write(item.key + '=' + item.trans + '\n')
+        outstr = item.key + '=' + item.trans
+        fhnd.write(from_unicode(outstr) + '\n')
 
 if __name__ == "__main__":
     #with open('msg_bundle_hu.properties.orig', 'rU') as fin:
     with open('msg_bundle_hu.properties', 'rU') as fin:
         items = propread(fin)
-        
-    print items['wholeday']
+
+    print items['Standings']
+    #print items['AICasual'].trans.decode('unicode-escape')
+    #print items['AICasual'].trans.decode('unicode-escape').encode('unicode-escape')
 
     with open('res', 'wb') as fout:
         propsave(fout, items)
