c901453836d6021e44d878449646244a3938254c
Andris <szell.andris@gmail.com>
2016-10-20 21:40:36 +0200

    Untranslated keys listing

--- translater.py
+++ translater.py
@@ -31,7 +31,7 @@ class TranslateWidget(Ui_TranslateWidget):
 
         self.origfname = origname
         self.transfname = transname
-        self.fill_dict(origname, transname)
+        self.create_dict(origname, transname)
 
         self.setup_status_bar()
         self.create_actions()
@@ -41,8 +41,11 @@ class TranslateWidget(Ui_TranslateWidget):
     def setup_status_bar(self):
         self.statusBar.showMessage('Translated:')
 
+        self.progressText = QtGui.QLabel('')
         self.progressBar = QtGui.QProgressBar()
+        self.statusBar.addPermanentWidget(self.progressText)
         self.statusBar.addPermanentWidget(self.progressBar)
+
         self.progressBar.setGeometry(30, 40, 200, 25)
 
         self.update_status_bar()
@@ -51,14 +54,19 @@ class TranslateWidget(Ui_TranslateWidget):
         orig_keycnt = len(self.origins)
         trans_keycnt = len(self.trans)
         self.progressBar.setValue(trans_keycnt * 100 / orig_keycnt)
+        self.progressText.setText('%d / %d' % (trans_keycnt, orig_keycnt))
 
     def tableKeyPress(self, event):
+        ''' Key bindings are hacked so a TAB press on table does not go to next cell but
+            switch between the detailed bottom editbox and table. '''
         if event.key() == QtCore.Qt.Key_Tab:
             event.ignore()
         else:
             self.oldTableKeyPress(event)
 
     def transEditKeyPress(self, event):
+        ''' Bottom edit box should not accept TABs but instead window should switch to
+            translation table '''
         if event.key() == QtCore.Qt.Key_Tab:
             event.ignore()
         else:
@@ -73,13 +81,14 @@ class TranslateWidget(Ui_TranslateWidget):
         findShortcut.activated.connect(self.on_find)
 
         self.filterEdit.textChanged.connect(self.filter_proxy_model.setFilterFixedString)
+        self.untransOnlyBox.clicked.connect(self.on_untransbox)
         selMode = self.tableView.selectionModel()
         selMode.selectionChanged.connect(self.on_sel_changed)
 
         self.transEdit.textChanged.connect(self.bottom_data_changed)
 
     def on_save(self):
-        # todo: save currently edited, but unfinished item as well!
+        # todo: save currently edited, but unfinished item as well on CTRL+S!
         fout = open(self.transfname, 'wb')
         propsave(fout, self.trans)
         print "Saved"
@@ -87,10 +96,31 @@ class TranslateWidget(Ui_TranslateWidget):
     def on_find(self):
         self.filterEdit.setFocus()
 
+    def on_untransbox(self):
+        if self.untransOnlyBox.isChecked():
+            self.fill_model(include_translated=False)
+        else:
+            self.fill_model(include_translated=True)
+
+    def update_hidden_col(self, row):
+        ''' Updates the hidden fourth column used for filtering (all texts together)
+        :param topleft: model index for the row whose hidden column is to be updated
+        '''
+        item_key = self.model.item(row, 0)
+        item_orig = self.model.item(row, 1)
+        item_trans = self.model.item(row, 2)
+        itext = ' '.join([item_key.text(), item_orig.text(), item_trans.text()])
+        item_all = QtGui.QStandardItem(itext)
+        signalsBlocked = self.model.signalsBlocked()
+        self.model.blockSignals(True)
+        self.model.setItem(row, 3, item_all)
+        self.model.blockSignals(signalsBlocked)
+
     def table_data_changed(self, topleft, bottomright):
         key = self.model.item(topleft.row(), 0).text()
         translation = self.model.item(topleft.row(), 2).text()
         self.update_translation(key, translation)
+        self.update_hidden_col(topleft.row())
 
         # refresh the other view of the same data
         if not self.tablerefresh_from_bottom:
@@ -118,13 +148,20 @@ class TranslateWidget(Ui_TranslateWidget):
         self.update_status_bar()
 
     def bottom_data_changed(self):
+        # the way I get to model item from filtered table row is obviously overcomplicated
+        # fix it please...
         self.tablerefresh_from_bottom = True
         selected = self.tableView.selectedIndexes()
         if selected: # if nothing selected, text gets lost?
-            key = self.model.item(selected[0].row(), 0).text()
-            self.update_translation(key, self.transEdit.toPlainText())
+            fp = self.filter_proxy_model
+            model_idx = fp.mapToSource(fp.index(selected[0].row(), 0))
+            key = self.model.itemFromIndex(model_idx).text()
+            #self.update_translation(key, self.transEdit.toPlainText())
             # refresh the other view of the same data
-            self.update_table(self.transEdit.toPlainText())
+            #print 'bott-updatetable'
+            #self.update_table(self.transEdit.toPlainText())
+            print "Updating row", model_idx.row()
+            self.model.item(model_idx.row(), 2).setText(self.transEdit.toPlainText())
 
         self.tablerefresh_from_bottom = False
 
@@ -141,17 +178,44 @@ class TranslateWidget(Ui_TranslateWidget):
             self.transEdit.blockSignals(False)
             self.commentEdit.setPlainText(self.origins[key].comment)
 
-    def update_table(self, translation):
-        selected = self.tableView.selectedIndexes()
-        self.model.item(selected[0].row(), 2).setText(translation)
-
     def on_sel_changed(self, topleft, bottomright):
         modelindex = topleft.indexes()[0]
         keyindex = self.filter_proxy_model.index(modelindex.row(), 0)
         key = keyindex.data()
         self.update_bottom(key)
 
-    def fill_dict(self, origname, transname):
+    def fill_model(self, include_translated=True, **kwargs):
+        self.model.blockSignals(True)
+        self.model.clear()
+
+        row = 0
+        for idx, tr in enumerate(self.origins.values()):
+            # skip lines with existing translation if 'untranslated only' selected
+            if tr.key in self.trans and not include_translated:
+                continue
+
+            item_key = QtGui.QStandardItem(tr.key)
+            item_key.setFlags(item_key.flags() & ~QtCore.Qt.ItemIsEditable)
+            item_orig = QtGui.QStandardItem(tr.trans)
+            item_orig.setFlags(item_orig.flags() & ~QtCore.Qt.ItemIsEditable)
+
+            if tr.key in self.trans:
+                item_trans = QtGui.QStandardItem(self.trans[tr.key].trans)
+            else:
+                item_trans = QtGui.QStandardItem('')
+
+            item_all = QtGui.QStandardItem(' '.join([item_key.text(), item_orig.text(), item_trans.text()]))
+
+            self.model.setItem(row, 0, item_key)
+            self.model.setItem(row, 1, item_orig)
+            self.model.setItem(row, 2, item_trans)
+            self.model.setItem(row, 3, item_all)
+            row += 1
+
+        self.model.blockSignals(False)
+        self.tableView.reset()
+
+    def create_dict(self, origname, transname):
         forig = open(origname, 'rU')
         ftrans = open(transname, 'rU')
         self.origins = propread(forig)
@@ -160,22 +224,7 @@ class TranslateWidget(Ui_TranslateWidget):
         self.model = QtGui.QStandardItemModel(5, 3)
         self.model.setHorizontalHeaderLabels(['ID', 'English', 'Translated'])
 
-        for idx, tr in enumerate(self.origins.values()):
-            item_key = QtGui.QStandardItem(tr.key)
-            item_key.setFlags(item_key.flags() & ~QtCore.Qt.ItemIsEditable)
-            item_orig = QtGui.QStandardItem(tr.trans)
-            item_orig.setFlags(item_orig.flags() & ~QtCore.Qt.ItemIsEditable)
-            if tr.key in self.trans:
-                item_trans = QtGui.QStandardItem(self.trans[tr.key].trans)
-            else:
-                item_trans = QtGui.QStandardItem('')
-
-            item_all = QtGui.QStandardItem(' '.join([item_key.text(), item_orig.text(), item_trans.text()]))
-
-            self.model.setItem(idx, 0, item_key)
-            self.model.setItem(idx, 1, item_orig)
-            self.model.setItem(idx, 2, item_trans)
-            self.model.setItem(idx, 3, item_all)
+        self.fill_model()
 
         self.filter_proxy_model = QtGui.QSortFilterProxyModel()
         self.filter_proxy_model.setSourceModel(self.model)
@@ -184,11 +233,6 @@ class TranslateWidget(Ui_TranslateWidget):
 
         self.tableView.setModel(self.filter_proxy_model)
         self.tableView.setColumnHidden(3, True)
-
-        #print dir(self.tableView)
-        #funcs = dir(self.tableView)
-        #for f in funcs:
-        #    print f
 
         header = self.tableView.horizontalHeader()
         header.setResizeMode(0, QtGui.QHeaderView.ResizeToContents)
